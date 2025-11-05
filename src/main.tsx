e53426e4b82422049ebf221654a518ea9757c0db
kiranchaudhary18 <kiran.chaudhary.cg@gmail.com>
2025-11-05 22:39:13 +0530

    solve the error

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 // src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import 'primereact/resources/themes/saga-blue/theme.css'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
